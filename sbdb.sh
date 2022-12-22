a8a624a497af40b0cc213a0ea78f728b5cc8b24c
Ulix Dev <91633356+ulixtech@users.noreply.github.com>
2022-12-22 23:20:12 +0000

    Basic Changes

--- sbdb.sh
+++ sbdb.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 # Variable
-NOW=$(date +%Y%m%d%H%M%S)
+NOW=$(date +%d-%m-%Y-%H-%M)
 SQL_BACKUP=${NOW}_database.sql
 FILES_BACKUP=${NOW}_files.tar.gz
-DB_BACKUP_FILE_ROOT= /smartbackups/db
-$Logpath=/smartbackups/logs/backup_log.txt
-max_age=7
+DB_BACKUP_FILE_ROOT=/home/$USER/smartbackups/db
+Logpath=/home/$USER/smartbackups/logs/backup_log.txt
 
-DB_NAME=$(sed -n "s/define( *'DB_NAME', *'\([^']*\)'.*/\1/p" wp-config.php)
-DB_USER=$(sed -n "s/define( *'DB_USER', *'\([^']*\)'.*/\1/p" wp-config.php)
-DB_PASSWORD=$(sed -n "s/define( *'DB_PASSWORD', *'\([^']*\)'.*/\1/p" wp-config.php)
-DB_HOST=$(sed -n "s/define( *'DB_HOST', *'\([^']*\)'.*/\1/p" wp-config.php)
+DB_NAME="sangbadonline_wp_g8ig6" #Replace DB_NAME
+DB_USER="sangbadonline_wp_rafqr" #Replace DB_USER
+DB_PASSWORD="05nMQCKhB23#pQ%$" #Replace DB_Password
+DB_HOST="localhost"
 
 # Backup database
 mysqldump --add-drop-table -u$DB_USER -p$DB_PASSWORD -h$DB_HOST $DB_NAME > $DB_BACKUP_FILE_ROOT/$SQL_BACKUP 2>&1
 
-# Compress the database dump file
-gzip ../backups/$SQL_BACKUP
-
-# Backup the entire public directory
-tar -zcf ../backups/$FILES_BACKUP .
-
-# Delete old backups
-find $DB_BACKUP_FILE_ROOT -mtime +$max_age -exec rm {} \;
 
 #Log
-echo "Database backup created at $(date)" >> $Logpath
+echo "$SQL_BACKUP | Database backup created at $(date)" >> $Logpath
+
+echo "$SQL_BACKUP | Database backup created at $(date)"
