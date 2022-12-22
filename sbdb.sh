da03549a98b89aa06d3f6e0da5b2e85a2bd2d2f5
Ulix Dev <91633356+ulixtech@users.noreply.github.com>
2022-12-22 22:36:48 +0000

    log

--- sbdb.sh
+++ sbdb.sh
@@ -4,6 +4,8 @@ NOW=$(date +%Y%m%d%H%M%S)
 SQL_BACKUP=${NOW}_database.sql
 FILES_BACKUP=${NOW}_files.tar.gz
 DB_BACKUP_FILE_ROOT= /smartbackups/db
+$Logpath=/smartbackups/logs/backup_log.txt
+max_age=7
 
 DB_NAME=$(sed -n "s/define( *'DB_NAME', *'\([^']*\)'.*/\1/p" wp-config.php)
 DB_USER=$(sed -n "s/define( *'DB_USER', *'\([^']*\)'.*/\1/p" wp-config.php)
@@ -11,7 +13,7 @@ DB_PASSWORD=$(sed -n "s/define( *'DB_PASSWORD', *'\([^']*\)'.*/\1/p" wp-config.p
 DB_HOST=$(sed -n "s/define( *'DB_HOST', *'\([^']*\)'.*/\1/p" wp-config.php)
 
 # Backup database
-mysqldump --add-drop-table -u$DB_USER -p$DB_PASSWORD -h$DB_HOST $DB_NAME > ../backups/$SQL_BACKUP 2>&1
+mysqldump --add-drop-table -u$DB_USER -p$DB_PASSWORD -h$DB_HOST $DB_NAME > $DB_BACKUP_FILE_ROOT/$SQL_BACKUP 2>&1
 
 # Compress the database dump file
 gzip ../backups/$SQL_BACKUP
@@ -19,5 +21,8 @@ gzip ../backups/$SQL_BACKUP
 # Backup the entire public directory
 tar -zcf ../backups/$FILES_BACKUP .
 
-# Find the last 12 files in the directory and store their names in a file
-find /path/to/directory -type f -printf '%T+ %p\n' | sort | tail -12 > /tmp/last_12_files.txt
+# Delete old backups
+find $DB_BACKUP_FILE_ROOT -mtime +$max_age -exec rm {} \;
+
+#Log
+echo "Database backup created at $(date)" >> $Logpath
